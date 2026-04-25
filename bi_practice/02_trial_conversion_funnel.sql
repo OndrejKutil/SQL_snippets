bdd929b6d98f2956905470dc14a2ff2dd195b5da
Ondřej Kutil <kutil.ondrej@outlook.com>
2026-04-25 11:05:44 +0200

    second sql

--- bi_practice/02_trial_conversion_funnel.sql
+++ bi_practice/02_trial_conversion_funnel.sql
@@ -73,4 +73,71 @@
 -- =============================================================================
 
 -- YOUR SOLUTION BELOW:
+/* Prepare base table with all combinations of region x product */
+WITH base_table AS (
+    SELECT r.region, p.*
+    FROM (
+        SELECT DISTINCT region
+        FROM users
+    ) r
+    CROSS JOIN (
+        SELECT product_id, product_name
+        FROM products
+        GROUP BY 1, 2 -- Technically does not need to be here, as product_id should be unique. Safety net
+    ) p
+)
+/* Filter trial_start_date to Q3 2024 (Jul–Sep) and add user region */
+,trials_started_in_q3 AS (
+    SELECT 
+        t1.trial_id
+        ,t1.product_id
+        ,t1.user_id
+        ,t1.trial_start_date
+        ,u.region
+    FROM trials t1
+    LEFT JOIN users u ON t1.user_id = u.user_id
+    WHERE t1.trial_start_date >= '2024-07-01' AND t1.trial_start_date <= '2024-09-30'
+)
+/* Add started trials count to the base table */
+,total_trials_add AS (
+    SELECT 
+        b.region
+        ,b.product_id
+        ,b.product_name
+        ,COUNT(DISTINCT t.trial_id) AS "total_trials"
+    FROM base_table b
+    LEFT JOIN trials_started_in_q3 t ON t.product_id = b.product_id AND t.region = b.region
+    GROUP BY 1, 2, 3
+)
+/* First find, then add in another CTE */
+,were_trials_converted AS (
+    SELECT
+        t.region
+        ,t.product_id
+        ,COUNT(DISTINCT t.trial_id) AS "converted_trials"
+        ,ROUND(AVG(l.purchase_date - t.trial_start_date), 0) AS "avg_days_to_convert"
+    FROM trials_started_in_q3 t
+    INNER JOIN licenses l ON l.user_id = t.user_id AND l.product_id = t.product_id
+    WHERE l.license_type = 'new'
+        AND l.purchase_date >= t.trial_start_date
+        AND l.purchase_date - t.trial_start_date <= 45
+    GROUP BY 1, 2
+)
+/* Add converted trials amount */
+,converted_trials_add AS (
+    SELECT t1.*, t2.converted_trials, t2.avg_days_to_convert
+    FROM total_trials_add t1
+    LEFT JOIN were_trials_converted t2 ON t1.region = t2.region AND t1.product_id = t2.product_id
+)
+SELECT 
+    region,
+    product_id,
+    product_name,
+    total_trials,
+    COALESCE(converted_trials, 0) AS converted_trials, -- converts nulls to specified value, here 0
+    ROUND((COALESCE(converted_trials, 0) * 100.0 / total_trials), 1) AS "conversion_rate_pct", -- multiplies by 100.0 to prevent integer division dropping decimals
+    avg_days_to_convert
+FROM converted_trials_add
+WHERE total_trials >= 5
+ORDER BY conversion_rate_pct DESC
 
